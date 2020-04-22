96cf4a960cb99ce28aef76daf8463c1fe7ed5198
Rene Rivera <grafikrobot@gmail.com>
2020-04-22 07:36:02 -0500

    Fix llvm repo location spec.

--- .ci_playground/linux-cxx-install.sh
+++ .ci_playground/linux-cxx-install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Usage:
-# LLVM_REPO: LLVM repo to obtain clang binaries. Only needed for clang install.
+# LLVM_OS: LLVM OS release to obtain clang binaries. Only needed for clang install.
 # PACKAGES: Compiler packages to install.
 
 set -e
@@ -9,9 +9,9 @@ echo ">>>>>"
 echo ">>>>> APT: REPO.."
 echo ">>>>>"
 sudo -E apt-add-repository -y "ppa:ubuntu-toolchain-r/test"
-if test -n "${LLVM_REPO}" ; then
+if test -n "${LLVM_OS}" ; then
 	wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-	sudo -E apt-add-repository "deb http://apt.llvm.org/xenial/ ${LLVM_REPO} main"
+	sudo -E apt-add-repository "deb http://apt.llvm.org/${LLVM_OS}/ llvm-toolchain-${LLVM_OS} main"
 fi
 echo ">>>>>"
 echo ">>>>> APT: UPDATE.."
