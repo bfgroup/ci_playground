419fcc029672f0701a8712adc50f3d1a3bc8a125
Rene Rivera <grafikrobot@gmail.com>
2021-03-06 08:40:54 -0600

    Support no xcversion Xcode use.
    It's become untenable to support downloading Xcode installs from Apple.
    Their 2FA setup essentially blocks use of their services by CI systems.
    Hence this adds support for selecting among teh Xcodes that are already
    installed in the system.

--- .ci_playground/macos-xcode-install.sh
+++ .ci_playground/macos-xcode-install.sh
@@ -6,30 +6,55 @@
 # XCODE_INSTALL_PASSWORD = Password for that account.
 
 set -e
-sudo gem install xcode-install
-xcversion update
-echo ">>>>>"
-echo ">>>>> AVAILABLE.."
-echo ">>>>>"
-xcversion list
-echo ">>>>>"
-echo ">>>>> INSTALLL: Xcode ${XCODE_VERSION}.."
-echo ">>>>>"
-xcversion --verbose --no-progress --no-clean --no-show-release-notes install ${XCODE_VERSION}
-echo ""
-echo ">>>>>"
-echo ">>>>> INSTALL: CLI Tools.."
-echo ">>>>>"
-xcversion --verbose install-cli-tools
-echo ""
-echo ">>>>>"
-echo ">>>>> AVAILABLE.."
-echo ">>>>>"
-xcversion list
-echo ">>>>>"
-echo ">>>>> SELECT: Xcode ${XCODE_VERSION}.."
-echo ">>>>>"
-xcversion select ${XCODE_VERSION} --symlink
+
+xc_versions ()
+{
+	grep -E -e '[>]([[:digit:]]+[.]){1,2}([[:digit:]]+)' ${PWD}/Xcode*/Contents/version.plist | sed -r -e 's/^([^:]+)[:][^>]+[>]([^<]+).*/\1 \2/g'
+}
+
+xc_path ()
+{
+	xc_versions | grep -F "$1" | sed -r -e 's/[[:space:]]+[0-9.]+$//g'
+}
+
+if test -n "${XCODE_INSTALL_USER}" ; then
+	sudo gem install xcode-install
+	xcversion update
+	echo ">>>>>"
+	echo ">>>>> AVAILABLE.."
+	echo ">>>>>"
+	xcversion list
+	echo ">>>>>"
+	echo ">>>>> INSTALLL: Xcode ${XCODE_VERSION}.."
+	echo ">>>>>"
+	xcversion --verbose --no-progress --no-clean --no-show-release-notes install ${XCODE_VERSION}
+	echo ""
+	echo ">>>>>"
+	echo ">>>>> INSTALL: CLI Tools.."
+	echo ">>>>>"
+	xcversion --verbose install-cli-tools
+	echo ""
+	echo ">>>>>"
+	echo ">>>>> AVAILABLE.."
+	echo ">>>>>"
+	xcversion list
+	echo ">>>>>"
+	echo ">>>>> SELECT: Xcode ${XCODE_VERSION}.."
+	echo ">>>>>"
+	xcversion select ${XCODE_VERSION} --symlink
+else
+	pushd /Applications
+	echo ">>>>>"
+	echo ">>>>> AVAILABLE.."
+	echo ">>>>>"
+	xc_versions
+	echo ">>>>>"
+	echo ">>>>> SELECT: Xcode ${XCODE_VERSION}.."
+	echo ">>>>>"
+	XC_PATH=`xc_path "${XCODE_VERSION}"`
+	sudo ln -s "${XC_PATH}" Xcode.app
+	sudo xcode-select -s "${PWD}/Xcode.app"
+fi
 
 # Use, modification, and distribution are
 # subject to the Boost Software License, Version 1.0. (See accompanying
